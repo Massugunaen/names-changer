5c6cf7de1abdbb4087dd2fbccc58f98fa3e3cf0a
User <sashapitr@me.com>
2019-10-15 03:04:47 +0300

    {name} as list or like string

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ intermediary_file = open('intermediary_file.txt', 'w', encoding='utf-8')
 
 with open('names.txt', 'r', encoding='utf-8') as source_file:
     for line in source_file:
-        intermediary_file.write(line)
-        print(line)
+        name = line.split(' ')
+        # intermediary_file.write(str(name))  # checking what type of variable
+        intermediary_file.write(str(name[0] + '\n'))
 
 source_file.close()
+intermediary_file.close()
