3a549c231f4f7ce5d3be7bbe520fd0357ee1ed98
User <sashapitr@me.com>
2019-10-15 09:00:50 +0300

    done with conjuration and initials

---
+++ functlib_forCNI.py
@@ -0,0 +1,54 @@
+def get_last_letters_surname(id_surname):
+    last_letters = id_surname[-2:]
+    return last_letters
+
+
+def get_last_letter_surname(id_surname):
+    last_letters = id_surname[-1:]
+    return last_letters
+
+
+def conjugate_surname(id_surname):
+    string_to_return = id_surname
+    # Males:
+    if get_last_letters_surname(id_surname) == "ов":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ев":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ев":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ун":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ин":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ик":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ич":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ап":
+        string_to_return = id_surname + 'у'
+    if get_last_letters_surname(id_surname) == "ой":
+        id_surname = id_surname[:-1]
+        string_to_return = id_surname + 'му'
+    if get_last_letters_surname(id_surname) == "ий":
+        id_surname = id_surname[:-2]
+        string_to_return = id_surname + 'ому'
+
+    # Females
+    if get_last_letters_surname(id_surname) == "ва":
+        id_surname = id_surname[:-1]
+        string_to_return = id_surname + "ой"
+    if get_last_letters_surname(id_surname) == "ая":
+        id_surname = id_surname[:-2]
+        string_to_return = id_surname + "ой"
+    if get_last_letter_surname(id_surname) == "а":
+        id_surname = id_surname[:-1]
+        string_to_return = id_surname + "ой"
+    return string_to_return
+
+
+def get_initials(id_name, id_patronymic):
+    name_initial = id_name[:1]
+    patronymic_initial = id_patronymic[:1]
+    string_to_return = name_initial.upper() + '.' + patronymic_initial.upper() + '.'
+    return string_to_return
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# structure of my data base: {Surname, Name, Patronymic,\n}
-import functlib
+import functlib_forCNI
+import funclib_forR
 
-intermediary_file = open('intermediary_file.txt', 'w', encoding='utf-8')
+conjugated_names_initials_file = open('conjugated_names_initials.txt', 'w', encoding='utf-8')
+respected_names_file = open('respected_names.txt', 'w', encoding='utf-8')
 
 with open('names.txt', 'r', encoding='utf-8') as source_file:
     for line in source_file:
@@ -9,10 +10,11 @@ with open('names.txt', 'r', encoding='utf-8') as source_file:
         surname = ID[0]
         name = ID[1]
         patronymic = ID[2]
-        intermediary_file.write(str(functlib.conjugate_surname(surname)
-                                    + ' '
-                                    + functlib.get_initials(name, patronymic)
-                                    + '\n'))
+        conjugated_names_initials_file.write(str(functlib_forCNI.conjugate_surname(surname)
+                                                 + ' '
+                                                 + functlib_forCNI.get_initials(name, patronymic)
+                                                 + '\n'))
+        respected_names_file.write(str())
 
 source_file.close()
-intermediary_file.close()
+conjugated_names_initials_file.close()
