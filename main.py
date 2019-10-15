2eb5a42b4dd685a211c4c1361bd8e362e089a795
User <sashapitr@me.com>
2019-10-15 04:28:40 +0300

    first function written, now need to get index elements for surnames

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+# structure of my data base: {Surname, Name, Patronymic,\n}
+def get_last_letters_surname():
+    return None
+
+
+def conjugate_surname(id_surname):
+    return None
+
+
+def get_initials(id_name, id_patronymic):
+    name_initial = id_name[:1]
+    patronymic_initial = id_patronymic[:1]
+    string_to_return = name_initial.upper() + '.' + patronymic_initial + '.'
+    return string_to_return
+
+
 intermediary_file = open('intermediary_file.txt', 'w', encoding='utf-8')
 
 with open('names.txt', 'r', encoding='utf-8') as source_file:
     for line in source_file:
-        name = line.split(' ')
-        writedown_name = name[0]
-        last_letters = writedown_name[-2:]
+        ID = line.split(' ')
+        surname = ID[0]
+        name = ID[1]
+        patronymic = ID[2]
+        last_letters = surname[-2:]
         if last_letters == "ов":
-            intermediary_file.write(str(writedown_name + 'у' + '\n'))
-        
+            intermediary_file.write(str(surname + 'у ' + get_initials(name, patronymic) + '\n'))
+
 
 source_file.close()
 intermediary_file.close()
