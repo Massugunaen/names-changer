1c251e791abe82d042bf462bdd083bdf6503dda2
User <sashapitr@me.com>
2019-10-15 08:07:09 +0300

    foundation of functions created

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 # structure of my data base: {Surname, Name, Patronymic,\n}
-def get_last_letters_surname():
-    return None
-
-
-def conjugate_surname(id_surname):
-    return None
-
-
-def get_initials(id_name, id_patronymic):
-    name_initial = id_name[:1]
-    patronymic_initial = id_patronymic[:1]
-    string_to_return = name_initial.upper() + '.' + patronymic_initial + '.'
-    return string_to_return
-
+import functlib
 
 intermediary_file = open('intermediary_file.txt', 'w', encoding='utf-8')
 
@@ -22,10 +9,10 @@ with open('names.txt', 'r', encoding='utf-8') as source_file:
         surname = ID[0]
         name = ID[1]
         patronymic = ID[2]
-        last_letters = surname[-2:]
-        if last_letters == "ов":
-            intermediary_file.write(str(surname + 'у ' + get_initials(name, patronymic) + '\n'))
-
+        intermediary_file.write(str(functlib.conjugate_surname(surname)
+                                    + ' '
+                                    + functlib.get_initials(name, patronymic)
+                                    + '\n'))
 
 source_file.close()
 intermediary_file.close()
